2a2b02782fd534c5d47e392a9a0a1103a1306640
Ivan Vasilev <vasilevivv@sibur.ru>
2026-04-14 23:20:40 +0300

    add operator translations

--- BlazorSvt/Components/Common/GenericGrid.razor.cs
+++ BlazorSvt/Components/Common/GenericGrid.razor.cs
@@ -8,21 +8,17 @@ namespace BlazorSvt.Components.Common;
 public partial class GenericGrid<TItem> : SvtComponentBase
 {
     private Grid<TItem> grid = default!;
-    private SettingsModal settingsModal = default!;
-
-    [Inject] 
-    public ILogger<GenericGrid<TItem>> Logger { get; set; } = default!;
-
-    [Inject] 
-    public IGridSettingsService<TItem> GridSettingsService { get; set; } = default!;
-
-    [Parameter, EditorRequired]
-    public GridDataProviderDelegate<TItem> DataProvider { get; set; } = default!;
-
-    [Parameter, EditorRequired]
-    public string PageTitle { get; set; } = default!;
 
     protected GridSettings<TItem>? GridSettings;
+    private SettingsModal settingsModal = default!;
+
+    [Inject] public ILogger<GenericGrid<TItem>> Logger { get; set; } = default!;
+
+    [Inject] public IGridSettingsService<TItem> GridSettingsService { get; set; } = default!;
+
+    [Parameter] [EditorRequired] public GridDataProviderDelegate<TItem> DataProvider { get; set; } = default!;
+
+    [Parameter] [EditorRequired] public string PageTitle { get; set; } = default!;
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
@@ -40,6 +36,7 @@ public partial class GenericGrid<TItem> : SvtComponentBase
         grid.ClearFilters();
         await grid.RefreshDataAsync();
     }
+
     private async Task ShowSettingsAsync()
     {
         await settingsModal.ShowAsync();
@@ -67,5 +64,27 @@ public partial class GenericGrid<TItem> : SvtComponentBase
         await GridSettingsService.ResetGridSettingsAsync(Lang);
         GridSettings = await GridSettingsService.GetGridSettingsAsync(Lang);
         StateHasChanged();
+    }
+
+    private async Task<IEnumerable<FilterOperatorInfo>> GridFiltersTranslationProvider()
+    {
+        var filters = new List<FilterOperatorInfo>
+        {
+            // Текстовые фильтры
+            new("*a*", L["FilterOperator.Contains"], FilterOperator.Contains),
+            new("!*a*", L["FilterOperator.DoesNotContain"], FilterOperator.DoesNotContain),
+            new("a**", L["FilterOperator.StartsWith"], FilterOperator.StartsWith),
+            new("**a", L["FilterOperator.EndsWith"], FilterOperator.EndsWith),
+            new("=", L["FilterOperator.Equals"], FilterOperator.Equals),
+            new("!=", L["FilterOperator.NotEquals"], FilterOperator.NotEquals),
+            // Числовые фильтры и даты
+            new("<", L["FilterOperator.LessThan"], FilterOperator.LessThan),
+            new("<=", L["FilterOperator.LessThanOrEquals"], FilterOperator.LessThanOrEquals),
+            new(">", L["FilterOperator.GreaterThan"], FilterOperator.GreaterThan),
+            new(">=", L["FilterOperator.GreaterThanOrEquals"], FilterOperator.GreaterThanOrEquals),
+            // Общие
+            new("x", L["FilterOperator.Clear"], FilterOperator.Clear)
+        };
+        return await Task.FromResult(filters);
     }
 }
