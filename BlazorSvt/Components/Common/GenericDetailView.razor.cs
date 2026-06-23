1439da8037b29aac94af49f4c83baccce1c8f0c5
Ivan Vasilev <vasilevivv@sibur.ru>
2026-06-23 20:55:03 +0300

    исправление риска связанного с потокобезопасностью избыточного кеширование - риск 5

--- BlazorSvt/Components/Common/GenericDetailView.razor.cs
+++ BlazorSvt/Components/Common/GenericDetailView.razor.cs
@@ -15,15 +15,10 @@ public partial class GenericDetailView<TItem, TDetailItem> : SvtComponentBase
     [Parameter] 
     public TItem Item { get; set; } = default!;
     [Parameter] 
-    public Func<TItem, object> KeySelector { get; set; } = default!;
-    [Parameter] 
     public Func<TItem, Task<TDetailItem>> DetailDataProvider { get; set; } = default!;
 
     [Parameter] 
     public RenderFragment<TDetailItem>? Template { get; set; }
-
-
-    private static readonly Dictionary<object, TDetailItem> ItemsCache = new();
 
     private TDetailItem? detail;
     private bool isLoading;
@@ -34,22 +29,11 @@ public partial class GenericDetailView<TItem, TDetailItem> : SvtComponentBase
         if (Item is null)
             return;
 
-        var key = KeySelector(Item);
-
         try
         {
             isLoading = true;
             errorMessage = null;
-            if (ItemsCache.TryGetValue(key, out var cached))
-            {
-                detail = cached;
-            }
-            else
-            {
-                detail = await DetailDataProvider(Item);
-                ItemsCache[key] = detail;
-            }
-            
+            detail = await DetailDataProvider(Item);
             detailSettings ??= DetailSettingsService.GetGridDetailSettings(Lang);
         }
         catch (Exception ex)
